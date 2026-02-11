073736d52cb3929df0f019391947ab40b2662afe
Deepti Wani <deepti.wani@agce.edu.in>
2026-02-11 13:13:12 +0530

    Delete Python Prog directory

--- Python Prog/Expt 1/3/swap2.py
+++
@@ -1,9 +0,0 @@
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-
-# Swapping without a temporary variable
-a, b = b, a
-
-print("After swapping:")
-print("a =", a)
-print("b =", b)
--- Python Prog/Expt 4/2/strslice.py
+++
@@ -1,8 +0,0 @@
-# Take input from the user
-string = input("Enter a string: ")
-
-# Reverse the string using slicing
-reversed_string = string[::-1]
-
-# Print the result
-print("Reversed string:", reversed_string)
